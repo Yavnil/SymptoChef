346edee50878b1b5c3b5fb50592dc33e688e7ce8
Yavnil <ayavnia@gmail.com>
2023-07-31 12:42:36 -0700

    Initial Commit

---
+++ SymptoChef/SymptoChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymptoChefApp.swift
+//  SymptoChef
+//
+//  Created by Al Yavnilovitch on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct SymptoChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
